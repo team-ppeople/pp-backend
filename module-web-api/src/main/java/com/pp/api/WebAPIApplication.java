0b56750e528fa7c40c188fe7a1fea6ba80443207
sinbom <dev.sinbom@gmail.com>
2024-04-02 10:41:54 +0900

    Initial commit

---
+++ module-web-api/src/main/java/com/pp/api/WebAPIApplication.java
@@ -0,0 +1,13 @@
+package com.pp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAPIApplication.class, args);
+    }
+
+}
