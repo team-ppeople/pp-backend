c22cf878002986108b1dfe99a39c913de3bfd25d
sinbom <dev.sinbom@gmail.com>
2024-05-01 17:15:32 +0900

    fix: Add post image file type check

--- module-web-api/src/main/java/com/pp/api/service/PostService.java
+++ module-web-api/src/main/java/com/pp/api/service/PostService.java
@@ -10,6 +10,7 @@ import com.pp.api.repository.UploadFileRepository;
 import com.pp.api.repository.UserRepository;
 import com.pp.api.service.command.CreatePostCommand;
 import com.pp.api.service.command.FindPostsByNoOffsetQuery;
+import com.pp.api.service.domain.CreatedPost;
 import com.pp.api.service.domain.PostOfList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Map;
 
+import static com.pp.api.entity.enums.UploadFileType.POST_IMAGE;
 import static com.pp.api.util.JwtAuthenticationUtil.checkUserPermission;
 import static com.pp.api.util.JwtAuthenticationUtil.getAuthenticatedUserId;
 import static java.util.function.Function.identity;
@@ -63,7 +65,7 @@ public class PostService {
     }
 
     @Transactional
-    public void create(CreatePostCommand command) {
+    public CreatedPost create(CreatePostCommand command) {
         User user = userRepository.findById(getAuthenticatedUserId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
 
@@ -91,6 +93,10 @@ public class PostService {
                         throw new IllegalArgumentException("업로드하지 않은 게시글 이미지가 존재합니다.");
                     }
 
+                    if (uploadFile.getFileType() != POST_IMAGE) {
+                        throw new IllegalArgumentException("게시글 이미지에 사용할 수 없는 이미지가 존재합니다.");
+                    }
+
                     checkUserPermission(uploadFile.getUploader().getId());
 
                     return PostImage.builder()
@@ -103,6 +109,16 @@ public class PostService {
         postsRepository.save(post);
 
         postImageRepository.saveAll(postImages);
+
+        return new CreatedPost(
+                post.getId(),
+                postImages.get(0)
+                        .getUploadFile()
+                        .getUrl(),
+                post.getTitle(),
+                post.getContent(),
+                post.getCreatedDate()
+        );
     }
 
 }
