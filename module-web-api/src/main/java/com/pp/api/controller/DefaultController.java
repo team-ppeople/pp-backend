7bef58653a1d3ab394134ba3e31031559f129ac7
sinbom <dev.sinbom@gmail.com>
2024-06-19 17:59:29 +0900

    refactor: Remove unnecessary endpoints

--- module-web-api/src/main/java/com/pp/api/controller/DefaultController.java
+++ module-web-api/src/main/java/com/pp/api/controller/DefaultController.java
@@ -1,7 +1,6 @@
 package com.pp.api.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +12,6 @@ public class DefaultController {
     @GetMapping(value = "/api/v1/healthcheck")
     public ResponseEntity<?> healthcheck() {
         return ok("OK");
-    }
-
-    @GetMapping(value = "/api/v1/permitall")
-    public ResponseEntity<?> permitall() {
-        return ok("permitall");
-    }
-
-    @PreAuthorize(value = "isAuthenticated()")
-    @GetMapping(value = "/api/v1/authenticated")
-    public ResponseEntity<?> authenticated() {
-        return ok("authenticated");
     }
 
 }
